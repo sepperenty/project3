576d109c8d0732bb131e5fe13b8166b5ae904fba
sepperenty <sepperenty@hotmail.com>
2016-09-28 17:48:12 +0200

    Laravel project + auth + migrations toegevoegd

---
+++ project3/app/User.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+
+
+    public function reactions()
+    {
+        return $this->hasMany(Reaction::class);
+    }
+
+     public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
