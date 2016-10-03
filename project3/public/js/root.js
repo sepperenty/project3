e5a67ae6878f0478d8e516287dd33dc70c29ec6f
sepperenty <sepperenty@hotmail.com>
2016-10-03 20:50:56 +0200

    Toevoegen wireframes

--- project3/public/js/root.js
+++ project3/public/js/root.js
@@ -2,7 +2,7 @@
 //     return window.location.href.match(/^.*\//)[0];
 // }
 var root = "http://localhost/Web_project_vrijwilligerswerk/project3/project3/public/";
-//var root = "http://localhost:8000/";
+var root = "http://localhost:8000/";
 function getBaseUrl() {
     var re = new RegExp(/^.*\//);
     return re.exec(window.location.href);
