7c994c7907802ade7e4d4852e54952e38fc84bb1
sepperenty <sepperenty@hotmail.com>
2016-10-05 15:17:14 +0200

    Email toegevoegd

--- project3/app/Http/Controllers/ProjectsController.php
+++ project3/app/Http/Controllers/ProjectsController.php
@@ -12,21 +12,20 @@ use App\Project;
 class ProjectsController extends Controller
 {
 
-	public function index()
-	{
+    public function index()
+    {
         return view('projects/index');
-	}
+    }
 
-	public function show(Project $project)
-	{
-		$project->load('reactions.user');
-		return view('projects/show', compact('project'));
+    public function show(Project $project)
+    {
+        $project->load('reactions.user');
+        return view('projects/show', compact('project'));
 
-	}
+    }
 
 
-
-	public function __construct()
+    public function __construct()
     {
         $this->middleware('auth');
     }
@@ -34,94 +33,79 @@ class ProjectsController extends Controller
 
     public function manage()
     {
-    	$projects = Project::all()->where('user_id', Auth()->user()->id);
+        $projects = Project::all()->where('user_id', Auth()->user()->id);
 
-    	return view('projects/manage', compact('projects'));
+        return view('projects/manage', compact('projects'));
 
     }
 
 
     public function store(Request $request)
     {
-    	 $this->validate($request, [
-        'title' => 'required|max:255',
-        'description' => 'required | max:500',
-        'address' => 'required | max:255',
-        'foto' 	=> 'max:5000 | mimes:jpeg,bmp,png'
-   			 ]);
+        $this->validate($request, [
+            'title' => 'required|max:255',
+            'description' => 'required | max:500',
+            'address' => 'required | max:255',
+            'foto' => 'max:5000 | mimes:jpeg,bmp,png'
+        ]);
 
 
-    	$project = new Project;
+        $project = new Project;
 
-    	$path = "";
+        $path = "";
 
-        if($request->hasFile('foto'))
-    	{
-    		$path = $request->foto->store('media/images');
-    		$project->foto = $path;
-    	}
-
-
-   		if($request->id)
-    	{
-    		$oldProject = Project::find($request->id);
-    		$user_id = $oldProject->user_id;
-
-    		if( $user_id == Auth()->user()->id )
-    		{
-    			if($path == "")
-    			{
-    				$path = $oldProject->foto;
-    			}
-
-    			$oldProject->update([
-
-    						'title'			=>	$request->title,
-    						'description'	=>	$request->description,
-    						'address'		=>	$request->address,
-    						'lat'			=>	$request->lat,
-    						'lng'			=>	$request->lng,
-    						'foto'			=> 	$path,
-
-    				]);
-    		}
-
-    		else
-    		{
-    			return "unauthorised";
-    		}
-    	}
-
-    	else
-    	{
-    		if($path == "")
-    		{
-    			$path = "media/images/default.jpeg";
-    			$project->foto = $path;
-    		}
-
-	    	$project->user_id = Auth()->user()->id;
-	    	$project->title = $request->title;
-	    	$project->description = $request->description;
-	    	$project->address = $request->address;
-	    	$project->lat = $request->lat;
-	    	$project->lng = $request->lng;
-    		$project->save();
-    	}
+        if ($request->hasFile('foto')) {
+            $path = $request->foto->store('media/images');
+            $project->foto = $path;
+        }
 
 
-    	return redirect('/projects/manage');
+        if ($request->id) {
+            $oldProject = Project::find($request->id);
+            $user_id = $oldProject->user_id;
+
+            if ($user_id == Auth()->user()->id) {
+                if ($path == "") {
+                    $path = $oldProject->foto;
+                }
+
+                $oldProject->update([
+
+                    'title' => $request->title,
+                    'description' => $request->description,
+                    'email' => $request->email,
+                    'telephoneNumber' => $request->telephoneNumber,
+                    'address' => $request->address,
+                    'lat' => $request->lat,
+                    'lng' => $request->lng,
+                    'foto' => $path,
+
+                ]);
+            } else {
+                return "unauthorised";
+            }
+        } else {
+            if ($path == "") {
+                $path = "media/images/default.jpeg";
+                $project->foto = $path;
+            }
+
+            $project->user_id = Auth()->user()->id;
+            $project->title = $request->title;
+            $project->description = $request->description;
+            $project->email = $request->email;
+            $project->telephoneNumber = $request->telephoneNumber;
+            $project->address = $request->address;
+            $project->lat = $request->lat;
+            $project->lng = $request->lng;
+            $project->save();
+        }
 
 
-
-
-
-
+        return redirect('/projects/manage');
 
 
     }
-
-
 
 
 }
