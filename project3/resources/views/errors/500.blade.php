78abc1456d650f85b6d75f1e2623dd3b69104cac
sepperenty <sepperenty@hotmail.com>
2016-11-04 15:09:17 +0100

    sync commit

--- project3/resources/views/errors/500.blade.php
+++ project3/resources/views/errors/500.blade.php
@@ -1,40 +1,50 @@
-@extends("layouts/app")
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Niet gevonden - Graag Gedaan</title>
+    <title>Document</title>
+     <link href="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css"
+          rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link href="/css/base.css" rel="stylesheet">
+</head>
+<body>
 
-	@section('header')
-		
-		<title>Niet gevonden - Graag Gedaan</title>
-
-	@endsection
-
-
-
-	@section('content')
-
-			  <div class="container-fluid ">
+    
+    
+              <div class="container-fluid ">
         <div class="row margin-top">
             <div class="col-md-10 col-md-offset-1 add_form">
                 <div class="row form-head">
                     <div class="col-md-12">
-                        <h1 class=""><span class="line-headers">F</span>out - Graag Gedaan </h1>
+                        <h1 class=""><span class="line-headers">P</span>agina niet gevonden </h1>
                     </div>
                 </div>
                 <div class="col-md-12 white-space">
 
-                <h1>Er is een fout opgetreden</h1>
+                <h1>De pagina die je zoekt bestaat niet.</h1>
 
-                <p>We lossen het zo snel mogelijk op.</p>
-					
-				
+                <p>Excuseer ons.</p>
+                    
+                
 
-				<a href="/" class="btn btn-default">Terug naar homepagina</a>
+                <a href="/" class="btn btn-primary">Terug naar homepagina</a>
 
 
                 </div>
                 </div>
                 </div>
                 </div>
+    
+</body>
+</html>
 
 
 
 
-	@endsection
+    
+
+
+
+
